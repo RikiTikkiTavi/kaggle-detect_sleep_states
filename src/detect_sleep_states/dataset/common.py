c395671d31a6efb5cd72956574059071818b706b
Yahor Paromau <yahor.paromau@gmail.com>
2023-11-17 23:41:27 +0100

    Base pipeline

---
+++ src/detect_sleep_states/dataset/common.py
@@ -0,0 +1,71 @@
+import numpy as np
+import polars as pl
+
+from detect_sleep_states.config import InferenceConfig, TrainConfig
+from detect_sleep_states.dataset.centernet import (
+    CenterNetTestDataset,
+    CenterNetTrainDataset,
+    CenterNetValidDataset,
+)
+from detect_sleep_states.dataset.detr import DETRTestDataset, DETRTrainDataset, DETRValidDataset
+from detect_sleep_states.dataset.seg import SegTestDataset, SegTrainDataset, SegValidDataset
+
+TRAIN_DATASET_TYPE = SegTrainDataset | DETRTrainDataset | CenterNetTrainDataset
+VALID_DATASET_TYPE = SegValidDataset | DETRValidDataset | CenterNetValidDataset
+TEST_DATASET_TYPE = SegTestDataset | DETRTestDataset | CenterNetTestDataset
+
+
+def get_train_ds(
+    cfg: TrainConfig,
+    event_df: pl.DataFrame,
+    features: dict[str, np.ndarray],
+) -> TRAIN_DATASET_TYPE:
+    if cfg.dataset.name == "seg":
+        return SegTrainDataset(cfg=cfg, features=features, event_df=event_df)
+    elif cfg.dataset.name == "detr":
+        return DETRTrainDataset(cfg=cfg, features=features, event_df=event_df)
+    elif cfg.dataset.name == "centernet":
+        return CenterNetTrainDataset(cfg=cfg, features=features, event_df=event_df)
+    else:
+        raise ValueError(f"Invalid dataset name: {cfg.dataset.name}")
+
+
+def get_valid_ds(
+    cfg: TrainConfig,
+    event_df: pl.DataFrame,
+    chunk_features: dict[str, np.ndarray],
+) -> VALID_DATASET_TYPE:
+    if cfg.dataset.name == "seg":
+        return SegValidDataset(
+            cfg=cfg,
+            chunk_features=chunk_features,
+            event_df=event_df,
+        )
+    elif cfg.dataset.name == "detr":
+        return DETRValidDataset(
+            cfg=cfg,
+            chunk_features=chunk_features,
+            event_df=event_df,
+        )
+    elif cfg.dataset.name == "centernet":
+        return CenterNetValidDataset(
+            cfg=cfg,
+            chunk_features=chunk_features,
+            event_df=event_df,
+        )
+    else:
+        raise ValueError(f"Invalid dataset name: {cfg.dataset.name}")
+
+
+def get_test_ds(
+    cfg: InferenceConfig,
+    chunk_features: dict[str, np.ndarray],
+) -> TEST_DATASET_TYPE:
+    if cfg.dataset.name == "seg":
+        return SegTestDataset(cfg=cfg, chunk_features=chunk_features)
+    elif cfg.dataset.name == "detr":
+        return DETRTestDataset(cfg=cfg, chunk_features=chunk_features)
+    elif cfg.dataset.name == "centernet":
+        return CenterNetTestDataset(cfg=cfg, chunk_features=chunk_features)
+    else:
+        raise ValueError(f"Invalid dataset name: {cfg.dataset.name}")
---
+++ src/detect_sleep_states/train.py
@@ -0,0 +1,95 @@
+import logging
+from pathlib import Path
+
+import hydra
+import torch
+from pytorch_lightning import Trainer, seed_everything
+from pytorch_lightning.callbacks import (
+    LearningRateMonitor,
+    ModelCheckpoint,
+    RichModelSummary,
+    RichProgressBar,
+)
+from pytorch_lightning.loggers import MLFlowLogger
+
+from detect_sleep_states.config import TrainConfig
+from detect_sleep_states.data_module import SleepDataModule
+from detect_sleep_states.model_module import PLSleepModel
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s:%(name)s - %(message)s"
+)
+_logger = logging.getLogger(__file__)
+
+
+@hydra.main(config_path="config", config_name="train", version_base="1.2")
+def main(cfg: TrainConfig):
+    seed_everything(cfg.seed)
+
+    # init lightning model
+    datamodule = SleepDataModule(cfg)
+    _logger.info("Set Up DataModule")
+    model = PLSleepModel(
+        cfg, datamodule.valid_event_df, len(cfg.features), len(cfg.labels), cfg.duration
+    )
+
+    # set callbacks
+    checkpoint_cb = ModelCheckpoint(
+        verbose=True,
+        monitor=cfg.trainer.monitor,
+        mode=cfg.trainer.monitor_mode,
+        save_top_k=1,
+        save_last=False,
+    )
+    lr_monitor = LearningRateMonitor("epoch")
+    progress_bar = RichProgressBar()
+    model_summary = RichModelSummary(max_depth=2)
+
+    # init experiment logger
+    pl_logger = MLFlowLogger(
+        experiment_name=cfg.exp_name
+    )
+
+    pl_logger.log_hyperparams(cfg)
+
+    trainer = Trainer(
+        # env
+        default_root_dir=Path.cwd(),
+        # num_nodes=cfg.training.num_gpus,
+        accelerator=cfg.trainer.accelerator,
+        precision=16 if cfg.trainer.use_amp else 32,
+        # training
+        fast_dev_run=cfg.trainer.debug,  # run only 1 train batch and 1 val batch
+        max_epochs=cfg.trainer.epochs,
+        max_steps=cfg.trainer.epochs * len(datamodule.train_dataloader()),
+        gradient_clip_val=cfg.trainer.gradient_clip_val,
+        accumulate_grad_batches=cfg.trainer.accumulate_grad_batches,
+        callbacks=[checkpoint_cb, lr_monitor, progress_bar, model_summary],
+        logger=pl_logger,
+        # resume_from_checkpoint=resume_from,
+        num_sanity_val_steps=0,
+        log_every_n_steps=int(len(datamodule.train_dataloader()) * 0.1),
+        sync_batchnorm=True,
+        check_val_every_n_epoch=cfg.trainer.check_val_every_n_epoch,
+    )
+
+    trainer.fit(model, datamodule=datamodule)
+
+    # load best weights
+    model = model.load_from_checkpoint(
+        checkpoint_cb.best_model_path,
+        cfg=cfg,
+        val_event_df=datamodule.valid_event_df,
+        feature_dim=len(cfg.features),
+        num_classes=len(cfg.labels),
+        duration=cfg.duration,
+    )
+    weights_path = str("model_weights.pth")  # type: ignore
+    _logger.info(f"Extracting and saving best weights: {weights_path}")
+    torch.save(model.model.state_dict(), weights_path)
+
+    return
+
+
+if __name__ == "__main__":
+    main()
